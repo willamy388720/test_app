3d33ae9e234c41ef6ae6a526782fc2487307b4ab
willamy388720 <willamygt@gmail.com>
2020-05-01 11:45:33 -0300

    Atributos Transitórios e Traits

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -11,5 +11,15 @@ RSpec.describe Customer, type: :model do
     expect(customer.vip).to eq(true)
   end
 
+  it 'Atributo Transitório' do
+    customer = create(:customer_default, upcased: true)
+    expect(customer.name.upcase).to eq(customer.name)
+  end
+
+  it 'Cliente Maculino Vip' do
+    customer = create(:customer_male_vip)
+    expect(customer.gender).to eq("M")
+  end
+
   it { expect{ create(:customer) }.to change{Customer.all.size}.by(1) }
 end
